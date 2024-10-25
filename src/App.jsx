c53d08a144a065d4bad825ccccf3d68b05f4fe8d
Jaber-Said <92320333+Jaber-Saed@users.noreply.github.com>
2024-10-25 18:03:13 +0300

    last thing

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
 
 function App() {
+  console.log("Made with love by the best software engineer Jaber!");
   return (
     <div className={styles.App}>
       <Navbar />
